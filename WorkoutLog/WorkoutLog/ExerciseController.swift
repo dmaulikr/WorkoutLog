d61cf9fb9931a5d17c93a989203d18a2a752b14b
Colton Lemmon <colton.lemmon@gmail.com>
2017-05-29 14:15:44 -0600

    Add ExerciseController.swift, add create and delete functions, add persistence.

---
+++ WorkoutLog/WorkoutLog/ExerciseController.swift
@@ -0,0 +1,50 @@
+//
+//  ExerciseController.swift
+//  WorkoutLog
+//
+//  Created by Colton Lemmon on 5/29/17.
+//  Copyright © 2017 Colton. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class ExerciseController {
+    
+    // MARK: - Shared Instance 
+    static let shared = ExerciseController()
+    
+    // MARK: - Internal Properties
+    var exercises: [Exercise] {
+        let request: NSFetchRequest<Exercise> = Exercise.fetchRequest()
+        let moc = CoreDataStack.context
+        do {
+            let result = try moc.fetch(request)
+            return result
+        } catch {
+            return []
+        }
+    }
+    
+    // MARK: - CRUD
+    func createExercise(name: String, sets: Int64?, reps: Int64?, weight: Double?) -> Exercise {
+        let sets = sets ?? 1
+        let reps = reps ?? 1
+        let weight = weight ?? 0.0
+        let exercise = Exercise(name: name, sets: sets, reps: reps, weight: weight, context: CoreDataStack.context)
+        saveToPersistentStorage()
+        return exercise
+    }
+    
+    func deleteExercise(exercise: Exercise) {
+        let moc = exercise.managedObjectContext
+        moc?.delete(exercise)
+        saveToPersistentStorage()
+    }
+    
+    //MARK: - Persistence
+    func saveToPersistentStorage() {
+        (try? CoreDataStack.context.save())
+    }
+    
+}
