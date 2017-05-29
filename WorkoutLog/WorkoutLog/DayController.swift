0cc5bcadca186fb2b8ffa08648dac5dd656946c3
Colton Lemmon <colton.lemmon@gmail.com>
2017-05-29 14:23:53 -0600

    Add DayController.swift, add create and delete funcitons, add persistence.

---
+++ WorkoutLog/WorkoutLog/DayController.swift
@@ -0,0 +1,45 @@
+//
+//  DayController.swift
+//  WorkoutLog
+//
+//  Created by Colton Lemmon on 5/29/17.
+//  Copyright © 2017 Colton. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class DayController {
+    
+    //MARK: - Shared Instance
+    
+    //MARK: - Internal Properties
+    var days: [Day] {
+        let request: NSFetchRequest<Day> = Day.fetchRequest()
+        let moc = CoreDataStack.context
+        do {
+            let result = try moc.fetch(request)
+            return result
+        } catch {
+            return []
+        }
+    }
+    //MARK: - CRUD
+    
+    func createDay(name: String) -> Day {
+        let day = Day(name: name, context: CoreDataStack.context)
+        //saveToPersistantStorage
+        return day
+    }
+    
+    func deleteDay(day: Day) {
+        let moc = day.managedObjectContext
+        moc?.delete(day)
+        //saveToPersistantStorage
+    }
+    
+    //MARK: - Persistence
+    func saveToPersistantStorage() {
+        (try? CoreDataStack.context.save())
+    }
+}
