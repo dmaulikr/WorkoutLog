a94dd1037691513767f7343ada6c6a92f4b640d6
Colton Lemmon <colton.lemmon@gmail.com>
2017-06-30 14:58:04 -0600

    Add ProgressHistoryViewController and tableView

---
+++ WorkoutLog/WorkoutLog/ProgressHistoryViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ProgressHistoryViewController.swift
+//  WorkoutLog
+//
+//  Created by Colton Lemmon on 6/30/17.
+//  Copyright © 2017 Colton. All rights reserved.
+//
+
+import UIKit
+
+class ProgressHistoryViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "exerciseCell", for: indexPath)
+        
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
