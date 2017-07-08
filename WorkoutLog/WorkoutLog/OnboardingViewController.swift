40cd4b5171fcedb3eeda50e4662649d1a7006d08
Colton Lemmon <colton.lemmon@gmail.com>
2017-07-07 19:33:26 -0600

    Add SwiftyWalkthrough cocoapod

---
+++ WorkoutLog/WorkoutLog/OnboardingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingViewController.swift
+//  WorkoutLog
+//
+//  Created by Colton Lemmon on 7/7/17.
+//  Copyright © 2017 Colton. All rights reserved.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
